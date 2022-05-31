c6b2d14a573b8de4d446c32aeeb7ca47a1c811a5
skoflok <skoflok@gmail.com>
2022-05-31 14:13:29 +0400

    implements cache

--- cache.go
+++ cache.go
@@ -1,23 +1,58 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Item struct {
+	value    string
+	deadline time.Time
+	infinite bool
+}
+
+func NewItem(value string, deadline time.Time, infinite bool) Item {
+	return Item{value: value, deadline: deadline, infinite: infinite}
+}
 
 type Cache struct {
+	store map[string]Item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]Item)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	item, ok := c.store[key]
+	if ok != true {
+		return "", false
+	}
 
+	if item.infinite == true {
+		return item.value, true
+	}
+
+	if item.deadline.Before(time.Now()) == true {
+		delete(c.store, key)
+		return "", false
+	}
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	newItem := NewItem(value, time.Now(), true)
+	c.store[key] = newItem
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0)
+	for k, _ := range c.store {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	newItem := NewItem(value, deadline, false)
+	c.store[key] = newItem
 }
